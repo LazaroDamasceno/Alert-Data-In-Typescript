2faff687643c86df75eae5dc25c6220e0ccd213d
lazarodamasceno <lazarolawrence2@gmail.com>
2022-12-10 18:20:30 -0300

    commit

---
+++ data.js
@@ -0,0 +1,3 @@
+function Data(fullName, age, id) {
+    alert("\n        Name: ".concat(fullName, "\n        Age: ").concat(age, "    \n        Id: ").concat(id, "\n    "));
+}
---
+++ data.ts
@@ -0,0 +1,9 @@
+function Data(fullName:string, age:number, id:number) : void {
+
+    alert(`
+        Name: ${fullName}
+        Age: ${age}    
+        Id: ${id}
+    `)
+
+}
